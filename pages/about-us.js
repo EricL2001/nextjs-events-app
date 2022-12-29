ec02f151aeb150fd851920e48e1c2009e826894e
Eric Levasseur <eric.levass@gmail.com>
2022-12-29 14:41:05 -0500

    removed apostrophes

--- pages/about-us.js
+++ pages/about-us.js
@@ -4,7 +4,7 @@ const AboutUsPage = () => {
 	return (
 		<div>
 			<h1> About Us </h1>
-			<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+			<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English.</p>
 			<h2> Next Section </h2>
 			<p>It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
 			</p>			
@@ -15,7 +15,7 @@ const AboutUsPage = () => {
 			  </li>
 			  <li className='about_us_list'>
 				<h3> Goober fantastical joyride </h3>
-					<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. </p>
+					<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text. </p>
 			  </li>
 			</ul>		
 		</div>
--- pages/events/event1.js
+++ pages/events/event1.js
@@ -2,7 +2,7 @@ const Page = () => {
 	return (
 		<div>
 			<h1> Event 1 Page </h1>
-			<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+			<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English.</p>
 		</div>
 		)
 }
