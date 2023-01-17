22a17f5a80577ab15fe5e87891b0137612855d27
Eric Levasseur <eric.levass@gmail.com>
2023-01-17 12:22:25 -0500

    fixed data.json path

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,8 @@ export default function Home({ data }) {
 
 //fetch the json data using server side call that will pass to the home function
 export async function getServerSideProps() {
-  const {events_categories} = await import('/data/data.json');
+  const {events_categories} = await import ('../data/data.json');
+  console.log({events_categories});
   return {
     props:{
       data: events_categories,
